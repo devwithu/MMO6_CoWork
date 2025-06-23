eb34fdf746d7967defb291007b74258750f094de
devwithu <devwithu@naver.com>
2025-06-23 11:25:09 +0900

    Db

---
+++ Server/Server/DB/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.DB
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<AccountDb> Accounts { get; set; }
+		public DbSet<PlayerDb> Players { get; set; }
+
+		static readonly ILoggerFactory _logger = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+		string _connectionString = @"Server=127.0.0.1;Database=mmo6;Uid=root;Pwd=;";
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options
+				.UseLoggerFactory(_logger)
+				//.UseMySQL(ConfigManager.Config == null ? _connectionString : ConfigManager.Config.connectionString);
+				.UseMySQL(_connectionString);
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<AccountDb>()
+				.HasIndex(a => a.AccountName)
+				.IsUnique();
+
+			builder.Entity<PlayerDb>()
+				.HasIndex(p => p.PlayerName)
+				.IsUnique();
+		}
+	}
+}
