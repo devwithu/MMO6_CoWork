1537979a7d06ac3fd233eff2421c61a910db2344
devwithu <devwithu@naver.com>
2025-06-25 19:46:27 +0900

    account server

---
+++ Server/AccountServer/DB/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountServer.DB
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<AccountDb> Accounts { get; set; }
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+
+		//string _connectionString = @"Server=base2.jdj.kr;Database=GameDB;User Id=sa;Password=mandlJa**18;Integrated Security=False;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		string _connectionString = @"Server=127.0.0.1;Database=account;Uid=root;Pwd=;";
+		
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options
+				//.UseLoggerFactory(_logger)
+				//.UseSqlServer(ConfigManager.Config == null ? _connectionString : ConfigManager.Config.connectionString);
+				.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+
+		}
+		
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<AccountDb>()
+				.HasIndex(a => a.AccountName)
+				.IsUnique();
+		}
+	}
+}
