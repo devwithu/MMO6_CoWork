a2a1b21d6532de63b0ddcb28f9df0c53e5f1e440
devwithu <devwithu@naver.com>
2025-06-25 15:25:24 +0900

    dummy 3

--- Client/Assets/Scripts/Managers/Contents/NetworkManager.cs
+++ Client/Assets/Scripts/Managers/Contents/NetworkManager.cs
@@ -21,7 +21,7 @@ public class NetworkManager
 		string host = Dns.GetHostName();
 		IPHostEntry ipHost = Dns.GetHostEntry(host);
 		IPAddress ipAddr = ipHost.AddressList[0];
-		IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 7777);
+		IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
 		Connector connector = new Connector();
 
